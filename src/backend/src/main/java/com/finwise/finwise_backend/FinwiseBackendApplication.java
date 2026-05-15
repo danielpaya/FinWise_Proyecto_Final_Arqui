cb09d648fb0942b4794b0533931dc10489f9df32
Daniel Sanchez Sotelo <88495391+dani11224@users.noreply.github.com>
2026-05-15 09:39:06 -0500

    feat: configuración inicial backend spring boot

---
+++ src/backend/src/main/java/com/finwise/finwise_backend/FinwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finwise.finwise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinwiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinwiseBackendApplication.class, args);
+	}
+
+}
